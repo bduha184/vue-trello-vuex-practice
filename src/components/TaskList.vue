f4dd866be55f7ece0ea5f03d9c2e1174bfb4913a
bduha184 <bduha_184@yahoo.co.jp>
2023-04-16 10:14:35 +0900

    list追加・削除

---
+++ src/components/TaskList.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="list">
+    <div class="listheader">
+      <p class="list-title">{{ title }}</p>
+      <div
+      class="deletelist"
+      @click="deleteList"
+      >
+      ×
+    </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props:{
+    title:{
+      type:String,
+      required:true
+    },
+    listIndex:{
+      type:Number,
+      required:true
+    },
+  },
+  methods:{
+    deleteList(){
+      if(confirm('本当に削除しますか？')){
+        this.$store.dispatch('deleteList',{listIndex:this.listIndex})
+      }
+    }
+  }
+};
+</script>
---
+++ src/components/TrelloBoard.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <header>my Trello</header>
+    <main>
+      <p class="info-line">All: 0 tasks</p>
+
+      <div class="list-index">
+        <TaskList
+          v-for="(list,index) in lists"
+          :key="list.id"
+          :title="list.title"
+          :listIndex="index"
+        />
+        <ListAdd />
+      </div>
+    </main>
+  </div>
+</template>
+
+<script>
+import ListAdd from "./ListAdd.vue";
+import TaskList from "./TaskList.vue";
+import { mapState } from "vuex";
+export default {
+  components: {
+    ListAdd,
+    TaskList,
+  },
+  computed:{
+    ...mapState([
+      'lists'
+    ])
+  }
+};
+</script>
